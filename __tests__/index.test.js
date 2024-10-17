e5a37bcdfffa77b21fd70c541ef7908f13039d24
e.kochetova <pandoraizida@gmail.com>
2024-10-17 15:29:53 +0200

    fixtures

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,23 @@
 import reverse from '../src/index.js';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { expect, test } from '@jest/globals';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFixtureFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 test('reverse', () => {
   expect(reverse('hello')).toEqual('olleh');
   expect(reverse('')).toEqual('');
+});
+
+test('reverse with log text', () => {
+  const text = readFixtureFile('fix_input.txt').trim();
+  const expected = readFixtureFile('fix_output.txt').trim();
+
+  expect(reverse(text)).toEqual(expected);
 });
